71c6175488b91c66a10fd04605c74002f72f624b
unknown <stm1998@hotmail.com>
2022-02-22 18:32:18 +0200

    update search controller checking if there is a fruit name Relates #14

--- src/controllers/searchFruits.js
+++ src/controllers/searchFruits.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 const searchFruits = (req, res) => {
   const fruitName = req.body.name;
@@ -7,12 +7,21 @@ const searchFruits = (req, res) => {
     .then((response) => response.json())
     .then((data) => (obj.data = data))
     .then((resp) => {
-      fetch(`https://api.codetabs.com/v1/proxy/?quest=https://imsea.herokuapp.com/api/10?q=${resp.name}`)
-        .then((response) => response.json())
-        .then((imageData) => {
-          obj.image = imageData.results[0];
-          res.send(obj);
-        });
+      if (resp.name) {
+        fetch(
+          `https://api.codetabs.com/v1/proxy/?quest=https://imsea.herokuapp.com/api/10?q=${resp.name}`
+        )
+          .then((response) => response.json())
+          .then((imageData) => {
+            obj.image = imageData.results[0];
+            res.send(obj);
+          });
+      } else {
+        res.send(obj);
+      }
+    })
+    .catch((err)=>{
+        res.status(404).send(err);
     });
 };
 module.exports = searchFruits;
