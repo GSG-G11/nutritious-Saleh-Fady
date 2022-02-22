a110a2ce62a24feaf84940e96268a16d26cdec97
Fady-Alwazir <fady.alwazir@gmail.com>
2022-02-22 20:33:55 +0200

    limting the data Relates #16

--- src/controllers/getFruites.js
+++ src/controllers/getFruites.js
@@ -3,8 +3,8 @@ const fetch = require('node-fetch');
 const getFruites = (req, res) => {
   fetch('https://www.fruityvice.com/api/fruit/all')
     .then((response) => response.json())
+    .then((data) => data.splice(0, 6))
     .then((data) => {
-      console.log(data);
       res.send(data);
     });
 };
